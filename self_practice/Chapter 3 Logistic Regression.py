37ccfa424860d6bc926deba0f8f3e05d34b02859
wei-nete <wei@nete.com>
2016-04-26 17:14:43 -0400

    04/26/2016, Logistic Regression

--- self_practice/Chapter 3 Logistic Regression.py
+++ self_practice/Chapter 3 Logistic Regression.py
@@ -7,18 +7,21 @@ X = iris.data[:, [2, 3]]
 y = iris.target
 
 from sklearn.cross_validation import train_test_split
-
+# random_state : int or RandomState
+# Pseudo-random number generator state used for random sampling.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-
+# everytime run it without specifying random_state, you will get a different result, this is expected behaviour.
+# If you use random_state=some_number, then you can guarantee that your split will be always the same.
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-sc.fit(X_train)
-X_train_std = sc.transform(X_train)
-X_test_std = sc.transform(X_test)
+sc.fit(X_train)  # only compute mean and std here
+X_train_std = sc.transform(X_train) # perform standardization by centering and scaling
+X_test_std = sc.transform(X_test) # perform standardization by centering and scaling
 
 from sklearn.linear_model import Perceptron
 
-ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0) # Check source code of Perceptron
+                                                                                     # Why return void.
 ppn.fit(X_train_std, y_train)
 
 y_pred = ppn.predict(X_test_std)
@@ -27,7 +30,6 @@ print 'Misclassified samples: %d' % (y_test != y_pred).sum()
 # calculate the classification accuracy of the perceptron on the test set as follows:
 from sklearn.metrics import accuracy_score
 print 'Accuracy: %.2f' % accuracy_score(y_test, y_pred)
-
 
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
@@ -56,15 +58,22 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
         X_test, y_test = X[test_idx, :], y[test_idx]
         plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidth=1, marker='o', s=55, label='test set')
 
-X_combined_std = np.vstack((X_train_std, X_test_std))
-y_combined = np.hstack((y_train, y_test))
+X_combined_std = np.vstack((X_train_std, X_test_std)) # Stack arrays in sequence horizontally (column wise).
+y_combined = np.hstack((y_train, y_test)) # Stack arrays in sequence vertically (row wise).
 plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105, 150))
+#######################Example ################################
+a = np.array([[1], [2], [3]])
+b = np.array([[2], [3], [4]])
+np.vstack((a,b))
+#array([[1], [2], [3], [2], [3], [4]])
+np.hstack((a,b))
+#array([[1, 2], [2, 3],[3, 4]])
+##############################################################
 
 plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.show()
-
 
 #############Modeling class probabilities via logistic regression###############
 ''' Plot the sigmoid function for some values in the range -7 to 7 to see what it looks like '''
