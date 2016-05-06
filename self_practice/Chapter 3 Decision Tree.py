0df16e0da0c17156bb2b352c5a45698d4a97661d
wei-Z <52lynn25@gmail.com>
2016-05-05 22:12:40 -0400

    05/05/2016, Chapter 3 Decision Tree

--- self_practice/Chapter 3 Decision Tree.py
+++ self_practice/Chapter 3 Decision Tree.py
@@ -37,4 +37,64 @@ plt.ylabel('Impurity Index')
 plt.show()
 
 # Building a decision tree
- 
+ '''
+ Decision trees can build complex decision boundaries by dividing the feature
+ space into rectangles. Using scikit-learn, we will now train a decision tree with
+ a maximum depth of 3 using entropy as a criterion for impurity.  Although feature
+ scaling may be desired for visualization purposes, note that feature scaling is
+ not a requirement for decision tree algorithms.
+ '''
+# Train a SVM model to classify the different flowers in our Iris dataset
+from sklearn import datasets
+import numpy as np
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+from sklearn.cross_validation import train_test_split
+
+# random_state : int or RandomState
+# Pseudo-random number generator state used for random sampling.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    # plot all samples
+    X_test, y_test = X[test_idx, :], y[test_idx]
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+
+    # highlight test samples
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidth=1, marker='o', s=55, label='test set')
+
+
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier(criterion='entropy',max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_train))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier=tree, test_idx=range(105,150))
+plt.xlabe('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
