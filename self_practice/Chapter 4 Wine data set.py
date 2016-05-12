907da6faafe347f5243c67793fd69a18f1966d27
wei-nete <wei@nete.com>
2016-05-12 16:50:37 -0400

    Chapter 4 05/12/2016

--- self_practice/Chapter 4 Wine data set.py
+++ self_practice/Chapter 4 Wine data set.py
@@ -35,7 +35,7 @@ mms = MinMaxScaler()
 X_train_norm = mms.fit_transform(X_train)
 X_test_norm = mms.transform(X_test)
 
-from sklearn.precessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 stdsc = StandardScaler()
 X_train_std = stdsc.fit_transform(X_train)
 X_test_std = stdsc.transform(X_test)
@@ -55,3 +55,70 @@ via feature selection.
 '''
 
 # Sparse solutions with L1 regularization
+# For regularized models in scikit-learn that support L1 regularization, we can simply
+# set the penalty parameter to 'l1' to yield the sparse solution.
+from sklearn.linear_model import LogisticRegression
+LogisticRegression(penalty='l1')
+'''
+Applied to the standardized Wine data, the L1 regularized logistic regression would
+yield the following sparse solution.
+'''
+lr = LogisticRegression(penalty='l1', C=0.1)
+lr.fit(X_train_std, y_train)
+print 'Training accuracy: ', lr.score(X_train_std, y_train)
+print 'Test accuracy: ', lr.score(X_test_std, y_test)
+
+'''
+Both training and test accuracies(both 98 percent) do not indicate any overfitting 
+of our model. When we access the intercept terms via the lr.intercept_attribute, 
+we can see that the array returns three values:
+'''
+lr.intercept_
+
+lr.coef_
+
+'''
+We noticed that the weight vectors are sparse, which means that they only have a 
+few non-zero entries. 
+'''
+
+'''
+Lastly, let's plot the regularization path, which is the weight coefficients of the different
+features fro different regularization strengths:
+'''
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = plt.subplot(lll)
+
+colors = ['blue', 'green', 'red', 'cyan',
+              'magenta', 'yellow', 'black',
+              'pink', 'lightgreen', 'lightblue',
+              'gray', 'indigo', 'orange']
+
+weights, params = [], []
+for c in np.arange(-4, 6):
+    lr = LogisticRegression(penalty='l1', C=10**c, random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column], label=df_wine.columns[column+1], color=color)
+    
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center', bbox_to_anchor=(1.38, 1.03), ncol=1, fancybox=True)
+plt.show()
+''' 
+The resulting plot provides us with further insights about the behavior of L1 regularization. 
+As we can see, all features weights will be zero if we penalizethe model with a strong 
+regularization parameter(C<0.1); C is the inverse of the regularization parameter lambda.
+'''
+
+    
